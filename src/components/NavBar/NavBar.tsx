11e29e253b8ef44a658839396e7494cde95b5d42
Anna Büchele <74052037+annabuechele@users.noreply.github.com>
2021-05-17 21:32:36 +0200

    button icon

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,19 +6,32 @@ import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import EventNoteIcon from "@material-ui/icons/EventNote";
+import Button from "@material-ui/core/Button";
+import { link } from "fs";
+import { IconButton } from "@material-ui/core";
 
 function NavBar() {
   return (
     <div className="NavBar-wrapper">
-      <MenuIcon id="hamburgerIcon"></MenuIcon>
+      <IconButton className="button-icon">
+        <MenuIcon></MenuIcon>
+      </IconButton>
       <div className="search-wrapper">
         <SearchIcon></SearchIcon>
       </div>
       <div className="icon-wrapper">
-        <ShoppingBasketIcon className="icons"></ShoppingBasketIcon>
-        <VisibilityIcon className="icons"></VisibilityIcon>
-        <EventNoteIcon className="icons"></EventNoteIcon>
-        <ExitToAppIcon className="icons"></ExitToAppIcon>
+        <IconButton className="button-icon">
+          <ShoppingBasketIcon className="icons"></ShoppingBasketIcon>
+        </IconButton>
+        <IconButton className="button-icon">
+          <VisibilityIcon className="icons"></VisibilityIcon>
+        </IconButton>
+        <IconButton className="button-icon">
+          <EventNoteIcon className="icons"></EventNoteIcon>
+        </IconButton>
+        <IconButton className="button-icon">
+          <ExitToAppIcon className="icons"></ExitToAppIcon>
+        </IconButton>
       </div>
     </div>
   );
